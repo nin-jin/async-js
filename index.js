59386f8a59fd248abef25e62948e2167ff238179
nin-jin <nin-jin@ya.ru>
2016-04-26 12:59:11 +0300

    Simple asynchronous application with fibers

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var Fiber = require( 'fibers' )
 var user = require( './user' )
 var greeter = require( './greeter' )
 
-greeter.say( 'Hello' , user )
-greeter.say( 'Bye' , user )
+Fiber( () => {
+    greeter.say( 'Hello' , user )
+    greeter.say( 'Bye' , user )
+} ).run()
--- user.js
+++ user.js
@@ -1,12 +1,14 @@
 var fs = require( 'fs' )
+var Future = require( 'fibers/future' )
 
 var config
 
 var getConfig = () => {
     if( config ) return config
 
-    var configText = fs.readFileSync( 'config.json' )
-    return config = JSON.parse( configText )
+    var configText = new Future
+    fs.readFile( 'config.json' , configText.resolver() )
+    return config = JSON.parse( configText.wait() )
 }
 
 module.exports.getName = () => {
