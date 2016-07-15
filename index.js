9135a9bfe24e8a792d540f03b7a40db7a182be9d
nin-jin <nin-jin@ya.ru>
2016-07-16 01:46:54 +0300

    x -> app

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var user = require( './user' )
 var greeter = require( './greeter' )
 
-async function x() {
+async function app() {
     console.time('time')
     await greeter.say('Hello', user)
     await greeter.say('Bye', user)
     console.timeEnd('time')
 }
 
-x()
+app()
