75310ba60b6330cbde3f46cc0554a8ba616441b9
nin-jin <nin-jin@ya.ru>
2016-07-16 00:54:09 +0300

    * Simple asynchronous application with generators and co

--- greeter.js
+++ greeter.js
@@ -1,3 +1,5 @@
-module.exports.say = ( greeting , user ) => {
-    console.log( greeting + ', ' + user.getName() + '!' )
-}
+var co = require( 'co' )
+
+module.exports.say = co.wrap( function* ( greeting , user ) {
+    console.log( greeting + ', ' + ( yield user.getName() ) + '!' )
+} )
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+var co = require( 'co' )
+
 var user = require( './user' )
 var greeter = require( './greeter' )
 
-console.time( 'time' )
-greeter.say( 'Hello' , user )
-greeter.say( 'Bye' , user )
-console.timeEnd( 'time' )
+co( function*() {
+    console.time( 'time' )
+    yield greeter.say( 'Hello' , user )
+    yield greeter.say( 'Bye' , user )
+    console.timeEnd( 'time')
+} ).catch( error => {
+    console.log( error )
+} )
+
