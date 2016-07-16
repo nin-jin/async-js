3aed156e9fbfba337b2b79c14ec0488dbaba2e27
nin-jin <nin-jin@ya.ru>
2016-07-16 13:26:52 +0300

    Throw error.

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var greeter = require( './greeter' )
 
 try {
 
-    console.time('time')
-    greeter.say('Hello', user)
-    greeter.say('Bye', user)
-    console.timeEnd('time')
+    console.time( 'time' )
+    greeter.say( 'Hello' , user )
+    greeter.say( 'Bye' , user )
+    console.timeEnd( 'time' )
+    greeter.say( 'Oops' , null )
 
 } catch( error ) {
     console.error( error )
